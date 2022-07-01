8512493716ad940145a7ad04bf95ffc70851ef90
Proverbes Affranchis Janel <janelaffranchis@gmail.com>
2022-07-01 14:46:13 +0000

    Adding Models, Controllers and Migrations

--- database/migrations/2022_07_01_124832_create_products_table.php
+++ database/migrations/2022_07_01_124832_create_products_table.php
@@ -13,14 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
-            $table->string('fname');
-            $table->string('lname');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
+            $table->string("label", length: 100)->unique();
+            $table->integer("stock");
             $table->timestamps();
         });
     }
@@ -32,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('products');
     }
 };
--- database/migrations/2022_07_01_131747_create_entries_table.php
+++ database/migrations/2022_07_01_131747_create_entries_table.php
@@ -13,14 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('entries', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
-            $table->string('fname');
-            $table->string('lname');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
+            $table->integer("quantity");
+            $table->integer("price");
+            $table->date("date");
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('entries');
     }
 };
